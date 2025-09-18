0a003782b4788702eb656532582ef16934f6ca5e
Riya Pandey <riyapandey2608@gmail.com>
2025-09-18 17:28:48 +0530

    fixed deployement error

--- ut-front-end/src/app/search/page.jsx
+++ ut-front-end/src/app/search/page.jsx
@@ -1,7 +1,7 @@
 import Wrapper from "@/layout/wrapper";
 // import HeaderTwo from "@/layout/headers/header-2";
 import CommonBreadcrumb from "@/components/breadcrumb/common-breadcrumb";
-import SearchArea from "@/components/search/search-area";
+// import SearchArea from "@/components/search/search-area";
 import UrbanThaliFooter from "@/layout/footers/urban-thali-footer";
 
 
@@ -12,9 +12,9 @@ export const metadata = {
 export default function SearchPage() {
   return (
     <Wrapper>
-      <HeaderTwo style_2={true} />
+      {/* <HeaderTwo style_2={true} /> */}
       <CommonBreadcrumb title="Search Products" subtitle="Search Products" />
-      <SearchArea />
+      {/* <SearchArea /> */}
       <UrbanThaliFooter />
     </Wrapper>
   );
